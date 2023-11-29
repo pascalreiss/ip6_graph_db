50edaa51bd37c7ef51fa951f4da06082ebea726b
delimarr <robin.delimar@students.fhnw.ch>
2023-11-29 17:00:14 +0000

    dab ring complete

--- infra_dab.py
+++ infra_dab.py
@@ -1,5 +1,7 @@
 """Neo4j Dachsburg-Ring example."""
+from pathlib import Path
 from typing import Dict, List, Tuple
+from uuid import uuid4
 
 import numpy as np
 from neo4j import GraphDatabase
@@ -11,41 +13,255 @@ from graph_db.data_elements.node_dc import Node
 from graph_db.data_elements.switch_item_enum import SwitchItem
 from graph_db.db_api import bidirectional_edge, double_node, reset_db
 
-w3_tp = Node(
-    id="w3_id",
-    ecos_id="3",
-    switch_item=SwitchItem.WEICHE,
-    name="w3",
-    bhf=Bhf.DAB,
-    coords=np.array([3, 3, 3]),
-)
 
+def guid() -> str:
+    """Get a random guid."""
+    return "guid_" + str(uuid4()).partition("-")[0]
 
-w_tps: List[Node] = [w3_tp]
-double_nodes: Dict[str, Tuple[Node, Node]] = {}
-query: List[str] = []
-
-for w_tp in w_tps:
-    cmd, nodes = double_node(w_tp)
-    double_nodes[w_tp.name] = nodes
-    query.append(cmd)
 
 URI = "bolt://neo4j:7687"
 USR = "neo4j"
 PASSWD = "password"
 
 driver = GraphDatabase.driver(URI, auth=(USR, PASSWD))
-
 session = driver.session()
-
 session.run(reset_db())
 
-for cmd in query:
+w_tps: List[Node] = []
+double_nodes: Dict[str, Tuple[Node, Node]] = {"DAB": {}, "ENS": {}, "CHA": {}}
+query: List[str] = []
+
+p = Path("./got_raw_files/")
+for file in p.glob("**/*.dat"):
+    bhf, name, dcc = file.name.split("_")
+    dcc = dcc.split(".")[0][3:]
+    node = Node(
+        id=guid(),
+        ecos_id=dcc,
+        switch_item=SwitchItem.WEICHE,
+        name=name,
+        bhf=Bhf[bhf.upper()],
+        coords=np.zeros((3,)),
+    )
+    w_tps.append(node)
+
+for w_tp in w_tps:
+    cmd, nodes = double_node(w_tp)
+    double_nodes[w_tp.bhf.name][w_tp.name] = nodes
+    query.append(cmd)
     session.run(cmd)
 
-edge_w0_w1 = Edge(
-    double_nodes["w0"][1], double_nodes["w1"][0], EdgeRelation.TRAIN_RAIL, 5
-)
+rails_dab = [
+    Edge(
+        source=double_nodes["DAB"]["ew01"][1],
+        dest=double_nodes["DAB"]["ew02"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew01"][1],
+        dest=double_nodes["DAB"]["ew04"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew02"][1],
+        dest=double_nodes["DAB"]["ew03"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew03"][1],
+        dest=double_nodes["DAB"]["ew04"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew03"][1],
+        dest=double_nodes["DAB"]["ew06"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew04"][1],
+        dest=double_nodes["DAB"]["ew05"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew05"][1],
+        dest=double_nodes["DAB"]["ew10"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew05"][1],
+        dest=double_nodes["DAB"]["ew10"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=2,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew06"][1],
+        dest=double_nodes["DAB"]["ew07"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew06"][1],
+        dest=double_nodes["DAB"]["ew09"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew07"][1],
+        dest=double_nodes["DAB"]["ew08"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew08"][1],
+        dest=double_nodes["DAB"]["ew09"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew09"][1],
+        dest=double_nodes["DAB"]["ew11"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew10"][1],
+        dest=double_nodes["DAB"]["ew12"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew11"][1],
+        dest=double_nodes["DAB"]["ew12"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew11"][1],
+        dest=double_nodes["DAB"]["ew14"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew13"][1],
+        dest=double_nodes["DAB"]["ew14"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+]
 
-cmd = bidirectional_edge(edge_w0_w1)
-session.run(cmd)
+rails_ens_cha = [
+    Edge(
+        source=double_nodes["CHA"]["ew01"][1],
+        dest=double_nodes["CHA"]["ew02"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew01"][1],
+        dest=double_nodes["CHA"]["ew04"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew01"][0],
+        dest=double_nodes["CHA"]["ew07"][1],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew02"][1],
+        dest=double_nodes["CHA"]["ew03"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew02"][0],
+        dest=double_nodes["CHA"]["ew08"][1],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew03"][1],
+        dest=double_nodes["CHA"]["ew04"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew05"][1],
+        dest=double_nodes["CHA"]["ew07"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew05"][1],
+        dest=double_nodes["ENS"]["w1"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew06"][1],
+        dest=double_nodes["CHA"]["ew08"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["CHA"]["ew06"][1],
+        dest=double_nodes["ENS"]["w2"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["ENS"]["w1"][1],
+        dest=double_nodes["ENS"]["w2"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+]
+
+rails_connect = [
+    Edge(
+        source=double_nodes["DAB"]["ew01"][0],
+        dest=double_nodes["CHA"]["ew03"][1],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew02"][0],
+        dest=double_nodes["CHA"]["ew04"][1],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew13"][1],
+        dest=double_nodes["CHA"]["ew06"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+    Edge(
+        source=double_nodes["DAB"]["ew14"][1],
+        dest=double_nodes["CHA"]["ew05"][0],
+        relation=EdgeRelation.TRAIN_RAIL,
+        distance=1,
+    ),
+]
+
+for rail in rails_dab:
+    cmd = bidirectional_edge(rail)
+    session.run(cmd)
+
+for rail in rails_ens_cha:
+    cmd = bidirectional_edge(rail)
+    session.run(cmd)
+
+for rail in rails_connect:
+    cmd = bidirectional_edge(rail)
+    session.run(cmd)
+
+session.close()
